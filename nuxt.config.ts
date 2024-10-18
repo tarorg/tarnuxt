9d7d04f3efe05adfb43556e1f28bcbbf6c48991a
wetarorg <tarframework@gmail.com>
2024-10-18 23:16:36 +0530

    tar

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+})
