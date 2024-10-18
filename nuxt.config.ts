f1be90c067857eb6478d1b88ed22102d5e7cb347
wetarorg <tarframework@gmail.com>
2024-10-19 00:26:01 +0530

    tar1.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', 'shadcn-nuxt', '@nuxtjs/tailwindcss'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
@@ -8,4 +19,5 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  compatibilityDate: '2024-10-18',
 })
--- pages/products.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <h1 class="text-3xl font-bold">
-      products
-    </h1>
-  </template>
